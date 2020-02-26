Prediction fix, normalization fix

components/prediction_dataset.py
@@ -22,7 +22,7 @@ class PredictionDataset(Dataset):
         self.settings = settings
         self.split = split
         # Get paths
-        nii_path = settings["paths"]["input_raw_path"]
+        self.nii_path = settings["paths"]["input_raw_path"]
 
         # create list
         nii_list = []
@@ -32,15 +32,20 @@ class PredictionDataset(Dataset):
         self.vdivs = -1
         self.offset_volume = -1
         # Load data
+        print(f"SPLIT {split}")
+        # exit()
         for item in split:
-            item_nii_path = os.path.join(nii_path, item)
-            image = torch.tensor(np.swapaxes(nib.load(item_nii_path).dataobj, 0, 1).astype(np.int64)).float()
+            item_nii_path = os.path.join(self.nii_path, item)
+            image = np.swapaxes(nib.load(item_nii_path).dataobj, 0, 1).astype(np.int64)
 
             if transform:
                 image = transform(image)
 
             if norm:
                 image = norm(image)
+
+            # Torchify
+            image = torch.tensor(image).float()
 
             if image.shape[0] > mb_size:
                 self.vdivs = find_divs(self.settings, image)
@@ -53,6 +58,23 @@ class PredictionDataset(Dataset):
             else:
                 nii_list.append(image.unsqueeze(1))
 
+            print(f"{item} {len(nii_list)}")
+
+
+        # Initiate single list
+        self.prediction_buffer_counter = -1
+        block_size = int(settings["dataloader"]["block_size"])
+        batch_size = int(settings["dataloader"]["batch_size"])
+        initial_shape = image.shape
+        print(f"Block size {block_size}\nBatch size {batch_size}\nInitial shape {initial_shape}")
+        required_batchsize = np.prod([i / block_size for i in initial_shape])
+        print(f"BATCHSIZE\t{batch_size}\nREQUIRED\t{required_batchsize}")
+        if batch_size < required_batchsize:
+            self.required_batchsize = required_batchsize
+            self.prediction_buffer = -1
+            self.prediction_buffer_size = int(required_batchsize / batch_size)
+            self.prediction_buffer_counter = 0
+            print(f"Buffer size {self.prediction_buffer_size}")
 
         self.item_list = nii_list
 
@@ -65,19 +87,40 @@ class PredictionDataset(Dataset):
     def save_item(self, idx, item):
         batch_size = int(self.settings["dataloader"]["batch_size"])
         output_path = self.settings["paths"]["output_seg_path"]
-        print(f"{idx} {item.shape}")
-        if not self.vdivs == -1:
-            # get volume from patch data 3d
-            item = np.squeeze(item)
-            direct_item = get_volume_from_patches3d(item, divs=self.vdivs)#, offset=self.offset_volume)
+
+        if self.prediction_buffer_counter > -1:
+            if self.prediction_buffer_counter == 0:
+                self.prediction_buffer = item
+            else:
+                self.prediction_buffer = np.concatenate([self.prediction_buffer, item], axis=0)
+            self.prediction_buffer_counter += 1
+            if self.prediction_buffer_counter == self.prediction_buffer_size:
+                #concat buffer, output image
+                self.prediction_buffer = np.concatenate([self.prediction_buffer, item], axis=0)
+                item = np.squeeze(self.prediction_buffer)
+                direct_item = get_volume_from_patches3d(item, divs=self.vdivs)
+                direct_item = direct_item.numpy().astype(np.int32)
+                # Required: ratio of required_batchsize 
+                item_index = int(idx / self.prediction_buffer_size)
+                direct_item_name = self.split[item_index]
+                print(f"Saving {idx} | {item_index} | {direct_item_name}") 
+                direct_output_path = os.path.join(output_path, direct_item_name)
+                write_nifti(direct_output_path, direct_item)
+                self.prediction_buffer = -1
+                self.prediction_buffer_counter = 0
         else:
-            # just save the volume 
-            direct_item = item
+            if not self.vdivs == -1:
+                # get volume from patch data 3d
+                item = np.squeeze(item)
+                direct_item = get_volume_from_patches3d(item, divs=self.vdivs)#, offset=self.offset_volume)
+            else:
+                # just save the volume 
+                direct_item = item
           
-        direct_item = direct_item.numpy().astype(np.int32)
-        direct_item_name = self.split[idx]
-        direct_output_path = os.path.join(output_path, direct_item_name)
-        write_nifti(direct_output_path, direct_item)
+            direct_item = direct_item.numpy().astype(np.int32)
+            direct_item_name = self.split[idx]
+            direct_output_path = os.path.join(output_path, direct_item_name)
+            write_nifti(direct_output_path, direct_item)
 
 
 

components/training_dataset.py
@@ -38,8 +38,8 @@ class TrainingDataset(Dataset):
             item_nii_path   = os.path.join(nii_path, item)
             item_gt_path    = os.path.join(gt_path, item)
 
-            image       = torch.tensor(np.swapaxes(nib.load(item_nii_path).dataobj, 0, 1).astype(np.int64)).float()
-            image_gt    = torch.tensor(np.swapaxes(nib.load(item_gt_path).dataobj, 0, 1).astype(np.int64)).float()
+            image       = np.swapaxes(nib.load(item_nii_path).dataobj, 0, 1).astype(np.int64)
+            image_gt    = np.swapaxes(nib.load(item_gt_path).dataobj, 0, 1).astype(np.int64)
 
             if transform:
                 image       = transform(image)
@@ -47,7 +47,11 @@ class TrainingDataset(Dataset):
 
             if norm:
                 image       = norm(image)
-                image_gt    = norm(image_gt)
+                # image_gt    = norm(image_gt)
+            
+            # Torchify
+            image = torch.tensor(image).float()
+            image_gt = torch.tensor(image_gt).float()
 
             if image.shape[0] > mb_size:
                 vdivs = find_divs(self.settings, image)

prediction/prediction.py
@@ -9,7 +9,7 @@ def prediction(settings):
     torch.cuda.init()
     torch.cuda.set_device(0)
     print("Predicting...")
-    input_list = os.listdir(settings["paths"]["input_raw_path"])
+    input_list = sorted(os.listdir(settings["paths"]["input_seg_path"]))
 
     output_path = settings["paths"]["output_seg_path"] + settings["paths"]["output_folder_prefix"] + " | " + str(datetime.datetime.now())
     os.mkdir(output_path)
@@ -27,8 +27,10 @@ def prediction(settings):
     sigmoid = settings["prediction"]["sigmoid"]
     binarize = settings["prediction"]["binarize"]
 
+    print(f"Len Loader {lenloader}")
     for idx, item in enumerate(loader):
-        print(f"Predicting item {idx} of {lenloader}\t", end="\r", flush=True)
+        # print(f"Predicting item {idx} of {lenloader}")
+        # print(f"Predicting item {idx} of {lenloader}\t", end="\r", flush=True)
         item_input  = item.cuda()
 
         logits = net(item_input)

utilities/loaders.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import utilities.normalization as normalization
+from functools import partial
 from torch.utils.data import DataLoader, BatchSampler, SequentialSampler, RandomSampler
 from functools import partial
 from components.deep_vessel_3d import Deep_Vessel_Net_FC
@@ -115,12 +117,25 @@ def load_network(settings, prediction=False):
 
         return net, criterion, optimizer, scheduler
 
+#TODO To get a better working system, rework the region data so that it can be used to get normalization values
+def get_normalization(settings):
+    """Return the normalization function used for image preprocessing
+    """
+    normalization_values = settings["preprocessing"]["normalization_values"]["foreground"]
+    min_data, max_data = float(normalization_values[0]), float(normalization_values[1])
+    cfreq = float(settings["preprocessing"]["cutoff_freq_high"])
+    scope = settings["preprocessing"]["normalization_type"]
+    norm_function = partial(normalization.histinfo, scope=scope, cfreq=cfreq, min_data=min_data, max_data=max_data)
+    return norm_function
+
 def get_loader(settings, input_list):
-    item_dataset = TrainingDataset(settings, input_list)
+    norm_function = get_normalization(settings)
+    item_dataset = TrainingDataset(settings, input_list, norm=norm_function)#=0.999))
     item_len = len(item_dataset)
     item_batch_size = item_len + 1
     if (item_len + 1) % int(settings["dataloader"]["batch_size"]) == 0 or item_batch_size > 5:
         item_batch_size = int(settings["dataloader"]["batch_size"])
+
     item_params = {
         "num_workers":int(settings["dataloader"]["num_workers"]),
         "pin_memory":True,
@@ -133,7 +148,8 @@ def get_loader(settings, input_list):
     return item_loader
 
 def get_prediction_loader(settings, input_list):
-    item_dataset = PredictionDataset(settings, input_list)
+    norm_function = get_normalization(settings)
+    item_dataset = PredictionDataset(settings, input_list, norm=norm_function)
     item_len = len(item_dataset)
     item_batch_size = item_len + 1
     if (item_len + 1) % int(settings["dataloader"]["batch_size"]) == 0 or item_batch_size > 5:

utilities/util.py
@@ -59,17 +59,17 @@ def get_volume_from_patches3d(patches4d, divs = (3,3,6), offset=(0,0,0)):
     """
     if "torch" in str(type(patches4d)):
         patches4d = patches4d.numpy()
-    print(f"Patches 4d shape {patches4d.shape}")
+    # print(f"Patches 4d shape {patches4d.shape}")
     new_shape = [(ps -of*2)*int(d) for ps, of, d in zip(patches4d.shape[-3:], offset, divs)]
     volume3d = np.zeros(new_shape, dtype=patches4d.dtype)
     shape = volume3d.shape
     widths = [int(s/d) for s, d in zip(shape, divs)]
     index = 0
-    print(f"Shape {shape} widths {widths}")
+    # print(f"Shape {shape} widths {widths}")
     for x in np.arange(0, shape[0], widths[0]):
         for y in np.arange(0, shape[1], widths[1]):
             for z in np.arange(0, shape[2], widths[2]):
-                print(f"X {x} Y {y} Z {z} index {index} {patches4d.shape}")
+                # print(f"X {x} Y {y} Z {z} index {index} {patches4d.shape}")
                 patch = patches4d[index,:,:,:]
                 index = index + 1
                 volume3d[x:x+widths[0],y:y+widths[1],z:z+widths[2]] = \