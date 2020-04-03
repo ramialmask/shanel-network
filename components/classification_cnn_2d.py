Fixed data loader isses: no more slicing of array; data types are now all float64; normalization using std implemented

components/classification_cnn_2d.py
@@ -16,7 +16,7 @@ class ClassificationCNN_2D(nn.Module):
     channels.
     """
 
-    def __init__(self, input_dim=(1, 300, 300), kernel_size=3,
+    def __init__(self, input_dim=(1, 400, 400), kernel_size=3,
                  stride_conv=1, pool=2, stride_pool=2,
                  num_classes=1):
         """
@@ -107,7 +107,7 @@ class ClassificationCNN_2D(nn.Module):
         height7 = int((height6 - kernel_size + 2 * padding + 1) / pool)#=4
         height8 = int((height7 - kernel_size + 2 * padding + 1) / pool)#=2
 
-        self.in_features = height7 * height7 # * 16 
+        self.in_features = height7 * height7  * 16 
         self.fc1 = nn.Linear(in_features=self.in_features, out_features=num_classes)
 
     def forward(self, x):

components/training_dataset_discriminator_2d.py
@@ -26,22 +26,23 @@ class TrainingDatasetDiscriminator_2D(Dataset):
         for item in split:
             item_image_path = os.path.join(image_path, item)
 
-            image       = np.array(Image.open(item_image_path)).astype(np.int64)[:100,:100]
+            image       = np.array(Image.open(item_image_path)).astype(np.float64)#[:100,:100]
             image_class = -1
             if "raw" in item:
                 image_class = 0
             elif "bg" in item:
                 image_class = 1
+            image_class = np.array(image_class).astype(np.float64)
 
             if transform:
                 image       = transform(image)
 
-            #TODO no norm for now until figured out
-            # if norm:
-            #     image       = norm(image)
+            if norm:
+                image       = norm(image)
             
             # Torchify
             image       = torch.tensor(image).float().unsqueeze(0)
+            image_class = torch.tensor(image_class).float()
 
             image_list.append((image, image_class))
 